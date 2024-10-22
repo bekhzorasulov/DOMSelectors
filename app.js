779566e507881b42148fcc2abaffd78026a51ac4
BekhzodRasulov <bekhzodr2001@gmail.com>
2024-10-22 13:11:13 +0500

    DOMSelectors

--- app.js
+++ app.js
@@ -48,3 +48,31 @@
 // Array.from(article.children).forEach((child) => {
 //     child.classList.add("article-child");
 // });
+
+// MODAL AKHROR--WEB
+
+const showBtn = document.getElementById("show-btn");
+const modal = document.getElementById("modal");
+const closeBtn = document.getElementById("close-btn");
+const overlay = document.getElementById("overlay");
+
+const addHidden = () => {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+const removeHidden = () => {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
+
+showBtn.addEventListener("click", removeHidden);
+
+closeBtn.addEventListener("click", addHidden);
+
+overlay.addEventListener("click", addHidden);
+
+document.addEventListener("keydown", (e) => {
+    if (e.key == "Escape") {
+        addHidden();
+    }
+});
